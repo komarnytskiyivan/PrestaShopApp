307c00910eae66909d9ecaa97e857eda14c4cfaa
komarnytskiyivan <komar.ivan.97@gmail.com>
2020-11-20 22:22:08 +0200

    Generated module

---
+++ var/cache/prod/smarty/cache/ps_featuredproducts/1/1/1/216/54/dc/15/54dc155ddd4642712d2a3045c51dde9b827a1e02.psfeaturedproductsviewste.php
@@ -0,0 +1,524 @@
+<?php
+/* Smarty version 3.1.33, created on 2020-11-19 16:19:32
+  from 'module:psfeaturedproductsviewste' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5fb67ef4e8bb89_21839285',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'fa6cc378d2942c8857b89d6bca728048c0caeedd' => 
+    array (
+      0 => 'module:psfeaturedproductsviewste',
+      1 => 1600952248,
+      2 => 'module',
+    ),
+    '4e48f9081812442e5797c9033049dad3e79d82e7' => 
+    array (
+      0 => '/var/www/html/themes/classic/templates/catalog/_partials/miniatures/product.tpl',
+      1 => 1600952248,
+      2 => 'file',
+    ),
+    'e64c699d70ea897ec9ed5b992b73872ad92542b6' => 
+    array (
+      0 => '/var/www/html/themes/classic/templates/catalog/_partials/variant-links.tpl',
+      1 => 1600952248,
+      2 => 'file',
+    ),
+  ),
+  'cache_lifetime' => 31536000,
+),true)) {
+function content_5fb67ef4e8bb89_21839285 (Smarty_Internal_Template $_smarty_tpl) {
+?><section class="featured-products clearfix">
+  <h2 class="h2 products-section-title text-uppercase">
+    Популярні товари
+  </h2>
+  <div class="products">
+          
+  <article class="product-miniature js-product-miniature" data-id-product="1" data-id-product-attribute="1" itemscope itemtype="http://schema.org/Product">
+    <div class="thumbnail-container">
+      
+                  <a href="http://localhost:8080/index.php?id_product=1&amp;id_product_attribute=1&amp;rewrite=hummingbird-printed-t-shirt&amp;controller=product#/1-rozmir-s/8-kolir-bilij" class="thumbnail product-thumbnail">
+            <img
+              src="http://localhost:8080/img/p/2/2-home_default.jpg"
+              alt="Hummingbird printed t-shirt"
+              data-full-size-image-url="http://localhost:8080/img/p/2/2-large_default.jpg"
+            />
+          </a>
+              
+
+      <div class="product-description">
+        
+                      <h3 class="h3 product-title" itemprop="name"><a href="http://localhost:8080/index.php?id_product=1&amp;id_product_attribute=1&amp;rewrite=hummingbird-printed-t-shirt&amp;controller=product#/1-rozmir-s/8-kolir-bilij">Hummingbird printed t-shirt</a></h3>
+                  
+
+        
+                      <div class="product-price-and-shipping">
+                              
+
+                <span class="sr-only">Базова ціна</span>
+                <span class="regular-price">28,68 ₴</span>
+                                  <span class="discount-percentage discount-product">-20%</span>
+                              
+              
+
+              <span class="sr-only">Ціна</span>
+              <span itemprop="price" class="price">22,94 ₴</span>
+
+              
+
+              
+            </div>
+                  
+
+        
+          
+        
+      </div>
+
+      <!-- @todo: use include file='catalog/_partials/product-flags.tpl'} -->
+      
+        <ul class="product-flags">
+                      <li class="product-flag discount">-20%</li>
+                      <li class="product-flag new">Новий</li>
+                  </ul>
+      
+
+      <div class="highlighted-informations hidden-sm-down">
+        
+          <a class="quick-view" href="#" data-link-action="quickview">
+            <i class="material-icons search">&#xE8B6;</i> Швидкий перегляд
+          </a>
+        
+
+        
+                      <div class="variant-links">
+      <a href="http://localhost:8080/index.php?id_product=1&amp;id_product_attribute=3&amp;rewrite=hummingbird-printed-t-shirt&amp;controller=product#/2-rozmir-m/8-kolir-bilij"
+       class="color"
+       title="Білий"
+              style="background-color: #ffffff"           ><span class="sr-only">Білий</span></a>
+      <a href="http://localhost:8080/index.php?id_product=1&amp;id_product_attribute=2&amp;rewrite=hummingbird-printed-t-shirt&amp;controller=product#/1-rozmir-s/11-kolir-chornij"
+       class="color"
+       title="Чорний"
+              style="background-color: #434A54"           ><span class="sr-only">Чорний</span></a>
+    <span class="js-count count"></span>
+</div>
+                  
+      </div>
+    </div>
+  </article>
+
+          
+  <article class="product-miniature js-product-miniature" data-id-product="2" data-id-product-attribute="9" itemscope itemtype="http://schema.org/Product">
+    <div class="thumbnail-container">
+      
+                  <a href="http://localhost:8080/index.php?id_product=2&amp;id_product_attribute=9&amp;rewrite=brown-bear-printed-sweater&amp;controller=product#/1-rozmir-s" class="thumbnail product-thumbnail">
+            <img
+              src="http://localhost:8080/img/p/2/1/21-home_default.jpg"
+              alt="Brown bear printed sweater"
+              data-full-size-image-url="http://localhost:8080/img/p/2/1/21-large_default.jpg"
+            />
+          </a>
+              
+
+      <div class="product-description">
+        
+                      <h3 class="h3 product-title" itemprop="name"><a href="http://localhost:8080/index.php?id_product=2&amp;id_product_attribute=9&amp;rewrite=brown-bear-printed-sweater&amp;controller=product#/1-rozmir-s">Hummingbird printed sweater</a></h3>
+                  
+
+        
+                      <div class="product-price-and-shipping">
+                              
+
+                <span class="sr-only">Базова ціна</span>
+                <span class="regular-price">43,08 ₴</span>
+                                  <span class="discount-percentage discount-product">-20%</span>
+                              
+              
+
+              <span class="sr-only">Ціна</span>
+              <span itemprop="price" class="price">34,46 ₴</span>
+
+              
+
+              
+            </div>
+                  
+
+        
+          
+        
+      </div>
+
+      <!-- @todo: use include file='catalog/_partials/product-flags.tpl'} -->
+      
+        <ul class="product-flags">
+                      <li class="product-flag discount">-20%</li>
+                      <li class="product-flag new">Новий</li>
+                  </ul>
+      
+
+      <div class="highlighted-informations no-variants hidden-sm-down">
+        
+          <a class="quick-view" href="#" data-link-action="quickview">
+            <i class="material-icons search">&#xE8B6;</i> Швидкий перегляд
+          </a>
+        
+
+        
+                  
+      </div>
+    </div>
+  </article>
+
+          
+  <article class="product-miniature js-product-miniature" data-id-product="3" data-id-product-attribute="13" itemscope itemtype="http://schema.org/Product">
+    <div class="thumbnail-container">
+      
+                  <a href="http://localhost:8080/index.php?id_product=3&amp;id_product_attribute=13&amp;rewrite=the-best-is-yet-to-come-framed-poster&amp;controller=product#/19-dimension-40x60cm" class="thumbnail product-thumbnail">
+            <img
+              src="http://localhost:8080/img/p/3/3-home_default.jpg"
+              alt="The best is yet to come&#039; Framed poster"
+              data-full-size-image-url="http://localhost:8080/img/p/3/3-large_default.jpg"
+            />
+          </a>
+              
+
+      <div class="product-description">
+        
+                      <h3 class="h3 product-title" itemprop="name"><a href="http://localhost:8080/index.php?id_product=3&amp;id_product_attribute=13&amp;rewrite=the-best-is-yet-to-come-framed-poster&amp;controller=product#/19-dimension-40x60cm">The best is yet to come&#039;...</a></h3>
+                  
+
+        
+                      <div class="product-price-and-shipping">
+              
+              
+
+              <span class="sr-only">Ціна</span>
+              <span itemprop="price" class="price">34,80 ₴</span>
+
+              
+
+              
+            </div>
+                  
+
+        
+          
+        
+      </div>
+
+      <!-- @todo: use include file='catalog/_partials/product-flags.tpl'} -->
+      
+        <ul class="product-flags">
+                      <li class="product-flag new">Новий</li>
+                  </ul>
+      
+
+      <div class="highlighted-informations no-variants hidden-sm-down">
+        
+          <a class="quick-view" href="#" data-link-action="quickview">
+            <i class="material-icons search">&#xE8B6;</i> Швидкий перегляд
+          </a>
+        
+
+        
+                  
+      </div>
+    </div>
+  </article>
+
+          
+  <article class="product-miniature js-product-miniature" data-id-product="4" data-id-product-attribute="16" itemscope itemtype="http://schema.org/Product">
+    <div class="thumbnail-container">
+      
+                  <a href="http://localhost:8080/index.php?id_product=4&amp;id_product_attribute=16&amp;rewrite=the-adventure-begins-framed-poster&amp;controller=product#/19-dimension-40x60cm" class="thumbnail product-thumbnail">
+            <img
+              src="http://localhost:8080/img/p/4/4-home_default.jpg"
+              alt="The adventure begins Framed poster"
+              data-full-size-image-url="http://localhost:8080/img/p/4/4-large_default.jpg"
+            />
+          </a>
+              
+
+      <div class="product-description">
+        
+                      <h3 class="h3 product-title" itemprop="name"><a href="http://localhost:8080/index.php?id_product=4&amp;id_product_attribute=16&amp;rewrite=the-adventure-begins-framed-poster&amp;controller=product#/19-dimension-40x60cm">The adventure begins Framed...</a></h3>
+                  
+
+        
+                      <div class="product-price-and-shipping">
+              
+              
+
+              <span class="sr-only">Ціна</span>
+              <span itemprop="price" class="price">34,80 ₴</span>
+
+              
+
+              
+            </div>
+                  
+
+        
+          
+        
+      </div>
+
+      <!-- @todo: use include file='catalog/_partials/product-flags.tpl'} -->
+      
+        <ul class="product-flags">
+                      <li class="product-flag new">Новий</li>
+                  </ul>
+      
+
+      <div class="highlighted-informations no-variants hidden-sm-down">
+        
+          <a class="quick-view" href="#" data-link-action="quickview">
+            <i class="material-icons search">&#xE8B6;</i> Швидкий перегляд
+          </a>
+        
+
+        
+                  
+      </div>
+    </div>
+  </article>
+
+          
+  <article class="product-miniature js-product-miniature" data-id-product="5" data-id-product-attribute="19" itemscope itemtype="http://schema.org/Product">
+    <div class="thumbnail-container">
+      
+                  <a href="http://localhost:8080/index.php?id_product=5&amp;id_product_attribute=19&amp;rewrite=today-is-a-good-day-framed-poster&amp;controller=product#/19-dimension-40x60cm" class="thumbnail product-thumbnail">
+            <img
+              src="http://localhost:8080/img/p/5/5-home_default.jpg"
+              alt="Today is a good day Framed poster"
+              data-full-size-image-url="http://localhost:8080/img/p/5/5-large_default.jpg"
+            />
+          </a>
+              
+
+      <div class="product-description">
+        
+                      <h3 class="h3 product-title" itemprop="name"><a href="http://localhost:8080/index.php?id_product=5&amp;id_product_attribute=19&amp;rewrite=today-is-a-good-day-framed-poster&amp;controller=product#/19-dimension-40x60cm">Today is a good day Framed...</a></h3>
+                  
+
+        
+                      <div class="product-price-and-shipping">
+              
+              
+
+              <span class="sr-only">Ціна</span>
+              <span itemprop="price" class="price">34,80 ₴</span>
+
+              
+
+              
+            </div>
+                  
+
+        
+          
+        
+      </div>
+
+      <!-- @todo: use include file='catalog/_partials/product-flags.tpl'} -->
+      
+        <ul class="product-flags">
+                      <li class="product-flag new">Новий</li>
+                  </ul>
+      
+
+      <div class="highlighted-informations no-variants hidden-sm-down">
+        
+          <a class="quick-view" href="#" data-link-action="quickview">
+            <i class="material-icons search">&#xE8B6;</i> Швидкий перегляд
+          </a>
+        
+
+        
+                  
+      </div>
+    </div>
+  </article>
+
+          
+  <article class="product-miniature js-product-miniature" data-id-product="6" data-id-product-attribute="0" itemscope itemtype="http://schema.org/Product">
+    <div class="thumbnail-container">
+      
+                  <a href="http://localhost:8080/index.php?id_product=6&amp;rewrite=mug-the-best-is-yet-to-come&amp;controller=product" class="thumbnail product-thumbnail">
+            <img
+              src="http://localhost:8080/img/p/6/6-home_default.jpg"
+              alt="Mug The best is yet to come"
+              data-full-size-image-url="http://localhost:8080/img/p/6/6-large_default.jpg"
+            />
+          </a>
+              
+
+      <div class="product-description">
+        
+                      <h3 class="h3 product-title" itemprop="name"><a href="http://localhost:8080/index.php?id_product=6&amp;rewrite=mug-the-best-is-yet-to-come&amp;controller=product">Mug The best is yet to come</a></h3>
+                  
+
+        
+                      <div class="product-price-and-shipping">
+              
+              
+
+              <span class="sr-only">Ціна</span>
+              <span itemprop="price" class="price">14,28 ₴</span>
+
+              
+
+              
+            </div>
+                  
+
+        
+          
+        
+      </div>
+
+      <!-- @todo: use include file='catalog/_partials/product-flags.tpl'} -->
+      
+        <ul class="product-flags">
+                      <li class="product-flag new">Новий</li>
+                  </ul>
+      
+
+      <div class="highlighted-informations no-variants hidden-sm-down">
+        
+          <a class="quick-view" href="#" data-link-action="quickview">
+            <i class="material-icons search">&#xE8B6;</i> Швидкий перегляд
+          </a>
+        
+
+        
+                  
+      </div>
+    </div>
+  </article>
+
+          
+  <article class="product-miniature js-product-miniature" data-id-product="7" data-id-product-attribute="0" itemscope itemtype="http://schema.org/Product">
+    <div class="thumbnail-container">
+      
+                  <a href="http://localhost:8080/index.php?id_product=7&amp;rewrite=mug-the-adventure-begins&amp;controller=product" class="thumbnail product-thumbnail">
+            <img
+              src="http://localhost:8080/img/p/7/7-home_default.jpg"
+              alt="Mug The adventure begins"
+              data-full-size-image-url="http://localhost:8080/img/p/7/7-large_default.jpg"
+            />
+          </a>
+              
+
+      <div class="product-description">
+        
+                      <h3 class="h3 product-title" itemprop="name"><a href="http://localhost:8080/index.php?id_product=7&amp;rewrite=mug-the-adventure-begins&amp;controller=product">Mug The adventure begins</a></h3>
+                  
+
+        
+                      <div class="product-price-and-shipping">
+              
+              
+
+              <span class="sr-only">Ціна</span>
+              <span itemprop="price" class="price">14,28 ₴</span>
+
+              
+
+              
+            </div>
+                  
+
+        
+          
+        
+      </div>
+
+      <!-- @todo: use include file='catalog/_partials/product-flags.tpl'} -->
+      
+        <ul class="product-flags">
+                      <li class="product-flag new">Новий</li>
+                  </ul>
+      
+
+      <div class="highlighted-informations no-variants hidden-sm-down">
+        
+          <a class="quick-view" href="#" data-link-action="quickview">
+            <i class="material-icons search">&#xE8B6;</i> Швидкий перегляд
+          </a>
+        
+
+        
+                  
+      </div>
+    </div>
+  </article>
+
+          
+  <article class="product-miniature js-product-miniature" data-id-product="8" data-id-product-attribute="0" itemscope itemtype="http://schema.org/Product">
+    <div class="thumbnail-container">
+      
+                  <a href="http://localhost:8080/index.php?id_product=8&amp;rewrite=mug-today-is-a-good-day&amp;controller=product" class="thumbnail product-thumbnail">
+            <img
+              src="http://localhost:8080/img/p/8/8-home_default.jpg"
+              alt="Mug Today is a good day"
+              data-full-size-image-url="http://localhost:8080/img/p/8/8-large_default.jpg"
+            />
+          </a>
+              
+
+      <div class="product-description">
+        
+                      <h3 class="h3 product-title" itemprop="name"><a href="http://localhost:8080/index.php?id_product=8&amp;rewrite=mug-today-is-a-good-day&amp;controller=product">Mug Today is a good day</a></h3>
+                  
+
+        
+                      <div class="product-price-and-shipping">
+              
+              
+
+              <span class="sr-only">Ціна</span>
+              <span itemprop="price" class="price">14,28 ₴</span>
+
+              
+
+              
+            </div>
+                  
+
+        
+          
+        
+      </div>
+
+      <!-- @todo: use include file='catalog/_partials/product-flags.tpl'} -->
+      
+        <ul class="product-flags">
+                      <li class="product-flag new">Новий</li>
+                  </ul>
+      
+
+      <div class="highlighted-informations no-variants hidden-sm-down">
+        
+          <a class="quick-view" href="#" data-link-action="quickview">
+            <i class="material-icons search">&#xE8B6;</i> Швидкий перегляд
+          </a>
+        
+
+        
+                  
+      </div>
+    </div>
+  </article>
+
+      </div>
+  <a class="all-product-link float-xs-left float-md-right h4" href="http://localhost:8080/index.php?id_category=2&amp;controller=category">
+    Усі товари<i class="material-icons">&#xE315;</i>
+  </a>
+</section>
+<?php }
+}
